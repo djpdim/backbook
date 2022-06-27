63c8fba36a481e7f95d79268713fa442376787ef
Dimosthenis Pagkas <dimosthenispagkas@MacBook-Pro-3.local>
2022-06-27 13:12:54 -0400

    update Thoughts model with the reaction Schema

--- models/Thoughts-model.js
+++ models/Thoughts-model.js
@@ -1,7 +1,36 @@
 const { Schema, model } = require("mongoose")
 // const dateFormat = require("../utils/dateFormat")
-const ReactionSchema = require("./Reaction-model")
 
+// reaction schema only
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal),
+        },
+    },
+    {
+        toJSON: {
+            getter: true,
+        },
+    }
+)
+
+// thought model
 const ThoughtsSchema = new Schema(
     {
         thoughtText: {
